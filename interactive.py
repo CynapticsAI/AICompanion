ad99bc2a7ddb3f6961a1d3f33fc543b51c01651f
Warlord-K <warlordk28@gmail.com>
2023-04-08 10:33:55 +0530

    Add Docstrings

--- interactive.py
+++ interactive.py
@@ -4,16 +4,44 @@ from transformers import pipeline,Conversation
 
 class AI_Companion:
     def __init__(self, asr = "openai/whisper-tiny", chatbot = "microsoft/DialoGPT-small", device = 0):
+        """
+        Create an Instance of the Companion.
+
+        Parameters:
+        asr: Huggingface ASR Model Card. Default: openai/whisper-tiny
+        chatbot: Huggingface Conversational Model Card. Default: microsoft/DialoGPT-small
+        device: Device to Run the model on. Default: 0 (GPU). Set to 1 to run on CPU.
+        """
         self.asr = pipeline("automatic-speech-recognition",model = asr,device=device)
         self.chatbot = pipeline("conversational", model = chatbot,device=device)
         self.chat = Conversation()
 
     def listen(self, audio, history):
+        """
+        Convert Speech to Text.
+
+        Parameters:
+        audio: Audio Filepath
+        history: Chat History
+
+        Returns:
+        history : history with recognized text appended
+        Audio : empty gradio component to clear gradio voice input
+        """
         text = self.asr(audio)["text"]
         history = history + [(text,None)]
         return history , None
     
     def respond(self, history):
+        """
+        Generates Response to User Input.
+
+        Parameters:
+        history: Chat History
+        
+        Returns:
+        history: history with response appended
+        """
         self.chat.add_user_input(history[-1][0])
         response = self.chatbot(self.chat)
         history[-1][1] = response.generated_responses[-1]
