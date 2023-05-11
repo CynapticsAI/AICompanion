d2d78f5772c276154703aff8506af38e9aabc6b1
Samip D Shah <95608626+snarkyidiot@users.noreply.github.com>
2023-05-11 13:45:40 +0530

    final Bot

--- interactive.py
+++ interactive.py
@@ -1,23 +1,29 @@
 import gradio as gr
 from transformers import pipeline,Conversation
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
-#ffmpeg
+from Person_bot import *
 
 class AI_Companion:
-    def __init__(self, asr = "openai/whisper-tiny", chatbot = "microsoft/DialoGPT-small", device = -1):
+    def __init__(self, asr = "openai/whisper-tiny", chatbot = "af1tang/personaGPT", device = -1,**kwargs):
         """
         Create an Instance of the Companion.
-
         Parameters:
         asr: Huggingface ASR Model Card. Default: openai/whisper-tiny
-        chatbot: Huggingface Conversational Model Card. Default: microsoft/DialoGPT-small
-        device: Device to Run the model on. Default: 0 (GPU). Set to 1 to run on CPU.
+        chatbot: Huggingface Conversational Model Card. Default: af1tang/personaGPT
+        device: Device to Run the model on. Default: -1 (GPU). Set to 1 to run on CPU.
         """
         self.asr = pipeline("automatic-speech-recognition",model = asr,device=device)
-        model = GPT2LMHeadModel.from_pretrained(chatbot)
-        tokenizer = GPT2Tokenizer.from_pretrained(chatbot)
-        self.chatbot = pipeline("conversational", model=model, tokenizer=tokenizer, device=device)
-        self.chat = Conversation()
+        self.model = GPT2LMHeadModel.from_pretrained(chatbot)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(chatbot)
+        self.personas=[]
+        self.dialog_hx=[]
+        self.sett={
+            "do_sample":True,
+            "top_k":10,
+            "top_p":0.92,
+            "max_length":1000,
+        }
+        # self.chat = Conversation()
 
     def listen(self, audio, history):
         """
@@ -34,8 +40,12 @@ class AI_Companion:
         text = self.asr(audio)["text"]
         history = history + [(text,None)]
         return history , None
-    
-    def respond(self, history):
+    def add_fact(self,audio):
+        text=self.asr(audio)
+        print(text)
+        self.personas.append(text['text']+self.tokenizer.eos_token)
+        return None
+    def respond(self, history,**kwargs):
         """
         Generates Response to User Input.
 
@@ -45,13 +55,17 @@ class AI_Companion:
         Returns:
         history: history with response appended
         """
-        self.chat.add_user_input(history[-1][0])
-        response = self.chatbot(self.chat)
-        print(response.generated_responses[-1])
-        history[-1][1] = response.generated_responses[-1]
+        print(self.personas)
+        personas = self.tokenizer.encode(''.join(['<|p2|>'] + self.personas + ['<|sep|>'] + ['<|start|>']))
+        # self.chat.add_user_input(history[-1][0])
+        user_inp= self.tokenizer.encode(history[-1][0]+self.tokenizer.eos_token)
+        self.dialog_hx.append(user_inp)
+        bot_input_ids = to_var([personas + flatten(self.dialog_hx)]).long()
+        full_msg =self.model.generate(bot_input_ids,do_sample=True,top_k=10,top_p=0.92,max_length=1000,pad_token_id=self.tokenizer.eos_token_id)
+        response = to_data(full_msg.detach()[0])[bot_input_ids.shape[-1]:]
+        self.dialog_hx.append(response)
+        history[-1][1] = self.tokenizer.decode(response, skip_special_tokens=True)
         return history
-
-
 bot = AI_Companion()
 
 def clear():
@@ -63,8 +77,8 @@ with gr.Blocks() as demo:
     with gr.Row():
         b1 = gr.Button("Submit")
         b2 = gr.Button("Clear")
-
+        b3=  gr.Button("Add Fact")
     b1.click(bot.listen, [audio, chatbot], [chatbot, audio]).then(bot.respond, chatbot, chatbot)
     b2.click(clear, [] , audio)
-
+    b3.click(bot.add_fact,[audio],[audio])
 demo.launch()
